15926e980c7d847e2a3d55756d11b24d5a1dd3b0
JC Grubbs <jc.grubbs@devmynd.com>
2012-07-16 14:53:38 -0500

    railtie

--- lib/serialize_with/railtie.rb
+++ lib/serialize_with/railtie.rb
@@ -3,7 +3,7 @@ module SerializeWith
     initializer "serialize_with.extend_active_record_base" do |app|
       if defined?(ActiveRecord)
         ActiveSupport.on_load(:active_record) do
-          self.include SerializeWith
+          ActiveRecord::Base.extend SerializeWith
         end
       end
     end
